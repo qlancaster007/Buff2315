1c1e8355ab8ede6f71102005f50c64a2eecbc3c4
qlancaster007 <123430570+qlancaster007@users.noreply.github.com>
2023-01-23 20:16:48 -0600

    Add files via upload
    Upload Homework 1

---
+++ Homework1.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        double x=2.5;
+        double y=3.3;
+        double z=4*x*x+3*y;
+        Console.WriteLine("x=2.5, y=3.3");
+        Console.WriteLine("The value of z is: "+z);
+    }
+}
